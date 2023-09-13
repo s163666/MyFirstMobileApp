2590ed78cd6ad59b0693034c9d4b83de0d009c4a
s163666 <harpreetbug@gmail.com>
2023-09-13 16:07:44 -0500

    did alot of hard work on xaml maui, creating base folders lots of effort put in

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MyFirstMobileApp
+﻿using MyFirstMobileApp.ViewViewModels.Main;
+
+namespace MyFirstMobileApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new MainView();
         }
     }
 }

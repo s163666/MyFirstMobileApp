c2bb73e588929bac01027fe4bbcfcf59a8181b40
s163666 <harpreetbug@gmail.com>
2023-09-21 16:18:26 -0500

    OnLayoutAsync created and ButtonStack layout page as well.

--- Models/TitleMain.cs
+++ Models/TitleMain.cs
@@ -10,13 +10,10 @@ namespace MyFirstMobileApp.Models
     {
         public static String appTitle { get; set; } = "Main Page View";
         //public string ButtonText { get; set; } = "Stack Layout";
-        public static String TitleStackLayout {get; set;} = "Stack Layout";
-
-        public static String TitleStackButton01 {get; set;} = "Stack Layout";
-        
-        public static String TitleStackButton02 {get; set;} = "Stack Layout";
-        
-        public static String TitleStackButton03 {get; set;} = "Stack Layout";
+        public static String TitleStackLayout { get; set; } = "Stack Layout";
+        public static String TitleButton01 { get; set; } = "Button 0, 1";
+        public static String TitleButton10 { get; set; } = "Button 1, 0";
+        public static String TitleButton11 { get; set; } = "Button 1, 1";
 
     }
 }
--- ViewViewModels/Main/MainViewModel.cs
+++ ViewViewModels/Main/MainViewModel.cs
@@ -1,16 +1,33 @@
 ﻿using MyFirstMobileApp.Models;
 using MyFirstMobileApp.ViewModels;
+using System.Windows.Input;
 
-namespace MyFirstMobileApp.ViewViewModels.Main
+namespace MyFirstMobileApp.ViewViewModels.Main;
+public class MainViewModel : BaseViewModel
 {
-    public class MainViewModel : BaseViewModel 
+    public string Layouts { get; set; }
 
+    public ICommand OnLayoutsClicked { get; set; }
+
+    public String StackLayoutTitle { get; set; } = TitleMain.TitleStackLayout;
+
+    public String Button01Title { get; set; } = TitleMain.TitleButton01;
+    public String Button10Title { get; set; } = TitleMain.TitleButton10;
+    public String Button11Title { get; set; } = TitleMain.TitleButton11;
+
+    public String Layouts { get; set; }
+
+    public MainViewModel()
     {
-        public String StackLayoutTitle { get; set; } = TitleMain.TitleStackLayout;
-        public MainViewModel()
-        {
-            Title = TitleMain.appTitle;
+        Title = TitleMain.appTitle;
 
-        }
+        //Set Commands
+        OnLayoutsClicked = new Command(OnLayoutsClickedAsync);
     }
+
+    private async void OnLayoutsClickedAsync()
+    {
+        await Application.Current.MainPage.Navigation.PushAsync(new StackLayoutView());
+    }
+
 }

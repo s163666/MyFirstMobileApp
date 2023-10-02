77bfa6652ff267b1f83512e9253676d8b62a06b2
s163666 <harpreetbug@gmail.com>
2023-10-02 16:17:15 -0500

    Finished stack layout box project.

--- Models/TitleLayout.cs
+++ Models/TitleLayout.cs
@@ -8,10 +8,12 @@ namespace MyFirstMobileApp.Models
 {
     public class TitleLayout
     {
+        //**Commented out the code below because it isn't NOT needed for this project**
+
         //Creates the databinding for the page StackLayoutView
-        public static String StackLayoutTitle { get; set; } = "Stack Layout";
-        public static String VerticalStackTitle { get; set; } = "Vertical Stack";
-        public static String HorizontalStackTitle { get; set; } = "Horizontal Stack";
-        public static String AbsoluteLayoutTitle { get; set; } = "Absolute Layout";
+        //public static String StackLayoutTitle { get; set; } = "Stack Layout";
+        //public static String VerticalStackTitle { get; set; } = "Vertical Stack";
+        //public static String HorizontalStackTitle { get; set; } = "Horizontal Stack";
+        //public static String AbsoluteLayoutTitle { get; set; } = "Absolute Layout";
     }
 }
